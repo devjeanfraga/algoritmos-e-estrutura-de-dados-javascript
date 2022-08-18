ea6c9cb8947295a683b2368cb76f9c6dfcff75a0
Jean Fraga <jeandevjs@gmail.com>
2022-08-18 15:11:50 -0300

    study: add recursion stringify numbers in nested object

---
+++ recursion-stringify-number.js
@@ -0,0 +1,27 @@
+function stringifyNumber (obj) {
+  let newObj = {}
+  for ( let key in obj ){
+      if (typeof obj[key] === 'number') {
+          newObj[key] = obj[key].toString();
+      } else if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+          newObj[key] = stringifyNumber(obj[key])
+      } else {
+          newObj[key] = obj[key]
+      }
+  }
+  return newObj; 
+}
+
+let obj = {
+  num: 1,
+  test: [],
+  data: {
+      val: 4,
+      info: {
+          isRight: true,
+          random: 66
+      }
+  }
+}
+
+stringifyNumber(obj)
