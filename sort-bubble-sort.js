a585ce42baf68e22760d5bdc9b4d327c47ef9702
Jean Fraga <jeandevjs@gmail.com>
2022-08-24 09:32:21 -0300

    study:  bubble-sort

---
+++ sort-bubble-sort.js
@@ -0,0 +1,16 @@
+/*
+  Bubble Sort 
+  eficiente para entrade de dados QUASE ORDENADA 
+*/
+
+
+/*
+  Requisitos 
+   - Swap | Trocar  
+*/
+
+/*
+  BIG-O NOTATION  
+    O(n) para listas quase ordenadas
+    O(n2)  para listas não ordenadas, por conta do aninhamento de loops 
+*/
