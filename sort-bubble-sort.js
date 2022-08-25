685ac168e7903b69327f4ca0d2f8c1a5d3bbb890
Jean Fraga <jeandevjs@gmail.com>
2022-08-25 07:59:22 -0300

    study:  add bubble sort

--- sort-bubble-sort.js
+++ sort-bubble-sort.js
@@ -1,6 +1,15 @@
 /*
+  SORT 
+*/
+
+const sortItems = ( a, b ) => a -b;
+const arrayUnsort = [3,7,90,2,9,6,1];
+const res = arrayUnsort.sort(sortItems)
+console.log(res)
+
+/*
   Bubble Sort 
-  eficiente para entrade de dados QUASE ORDENADA 
+  eficiente para entrada de dados QUASE ORDENADA 
 */
 
 
@@ -9,8 +18,36 @@
    - Swap | Trocar  
 */
 
+const swap1 = (arr, idx1,idx2) => [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
+const swap2 = (arr, idx1,idx2) => {
+  let temp = arr[idx1]
+  arr[idx1] = arr[idx2]
+  arr[idx2] = temp
+}
+
+
+function bubbleSort (arr) {
+  let isSwap
+  for (let i = arr.length; i > 0; i--) {
+    isSwap = false;
+    for (let j = 0; j < i -1 ; j++) {
+      if (arr[j] > arr[j+1]) 
+        swap2(arr, j, j+1)
+        isSwap = true 
+    }
+    if (isSwap) break; 
+  }
+  console.log(arr)
+  return arr;
+}
+
+bubbleSort([8,1,2,3,4,5,6,7]);
+
+
+
 /*
   BIG-O NOTATION  
     O(n) para listas quase ordenadas
     O(n2)  para listas não ordenadas, por conta do aninhamento de loops 
 */
+
