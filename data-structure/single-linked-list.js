b83bcc9f5aebb01561fcc190caad66baba701144
Jean Fraga <jeandevjs@gmail.com>
2022-09-05 18:59:03 -0300

    study: add insert method

--- data-structure/single-linked-list.js
+++ data-structure/single-linked-list.js
@@ -158,7 +158,35 @@ class SinglyLinkedList  {
       return false;   
     }
 
-   
+    /* 
+    INSERIR VALORES EM QUALQUER POSIÇÃO  DA LISTA
+    [Pseudo code]
+    
+    - If the index is less than zero or greather than the length, return false
+    - If the index is the same as the length, push a new node to the end of the list
+    - If the index is 0, unshift a new node to the start of the list 
+    - Otherwise, using the get method, access the node at the index -1
+    - Set the next property on that node to be the new node 
+    - Set the next property on the new node to be the previous next 
+    - Increment the length 
+    - Return true
+  */
+
+    insert (index, value) {
+      if (index < 0 || index > this.length ) return false;
+      if ( index === this.length ) return !!this.push(value);
+      if (index === 0 ) return !!this.unshift(value);
+
+      let newNode = new Node(value);
+      let prev = this.get(index-1);
+      let temp =  prev.next; 
+      prev.next = newNode;
+      newNode.next = temp;
+      this.length++;
+      return true; 
+    }
+
+
 
 
 }
