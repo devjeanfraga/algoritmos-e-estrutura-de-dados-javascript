0e15280b2bedcf12eef9f2888c7ae9b28088fa45
Jean Fraga <jeandevjs@gmail.com>
2022-09-05 11:33:59 -0300

    study: add unshift method

--- data-structure/single-linked-list.js
+++ data-structure/single-linked-list.js
@@ -110,6 +110,32 @@ class SinglyLinkedList  {
       if (this.length === 0 ) this.tail = null;
       return currentHead.value;
     }
+  
+    /* 
+    ADICIONAR VALORES NO INÍCIO DA LISTA
+    [Pseudo code]
+    
+    - This function should accept a value
+    - Create a new node using the value passed to the function 
+    - If there is no head property on the list, set the head and tail to be the newly create node
+    - Otherwise set the newly create node's next property to be the current head property on the list. 
+    - Set the head property on the list to be that newly created node
+    - Increment the length of the list by 1 
+    - Return the linked list 
+  */
+
+    unshift (value) {
+      let newNode = new Node(value);
+      if (!this.head) {
+        this.head = newNode;
+        this.tail =  this.head;
+      } else {
+        newNode.next = this.head;
+        this.head = newNode;
+      }
+      this.length++;
+      return this; //retorna a lista 
+    }
 
 }
 
