d322968e37fb79963a75e02e42dd116ed85df86a
Jean Fraga <jeandevjs@gmail.com>
2022-09-09 18:20:14 -0300

    study: add pop method

--- data-structure/doubly-linked-list.js
+++ data-structure/doubly-linked-list.js
@@ -27,21 +27,21 @@ class DoublyLinkedList {
     return this; 
   }
 
-  // pop () {
-  //   if (this.length === 0 ) return undefined;
-  //   let poppedNode = this.tail;
+  pop () {
+    if (this.length === 0 ) return undefined;
+    let poppedNode = this.tail;
 
-  //   if (this.length === 1 ) {
-  //     this.head = null;
-  //     this.tail = null;
-  //   } else {
-  //     this.tail = poppedNode.prev;
-  //     this.tail.next = null; 
-  //     poppedNode.prev =  null; 
-  //   }
-  //   this.length--;
-  //   return poppedNode; 
-  // }
+    if (this.length === 1 ) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null; 
+      poppedNode.prev =  null; 
+    }
+    this.length--;
+    return poppedNode; 
+  }
 
   // /*
   //   PSEUDOCODE
