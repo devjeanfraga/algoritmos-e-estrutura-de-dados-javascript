96ad1047fd07d022472479b70248617cb26e552e
Jean Fraga <jeandevjs@gmail.com>
2022-09-11 16:58:28 -0300

    study: add enqueue method

--- data-structure/queue-with-linked-list.js
+++ data-structure/queue-with-linked-list.js
@@ -10,17 +10,17 @@ class Queue {
     this.last = null;
     this.size = 0; 
   }
-  // enqueue (val) {
-  //   let newNode = new Node(val);
-  //   if (!this.first) {
-  //     this.first = newNode;
-  //     this.last = newNode;
-  //   } else {
-  //     this.last.next = newNode;
-  //     this.last = newNode;
-  //   }
-  //   return ++this.size;
-  // }
+  enqueue (val) {
+    let newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
   // dequeue () {
   //   if (!this.first) return null; 
   //   let temp = this.first; 
