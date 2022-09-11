2fee4a3efa432343e93b3b88f3a78860626b1f86
Jean Fraga <jeandevjs@gmail.com>
2022-09-11 15:33:44 -0300

    study:  add Stack

---
+++ data-structure/stack-with-linked-list.js
@@ -0,0 +1,32 @@
+/*
+  STACK
+  É um conceito de estrutura de dados 
+  onde o ultimo a entrar é o primeiro a sair. 
+  É o conceito usado na recursão, por exemplo. 
+*/
+
+class Node {
+  constructor (val) {
+    this.val = val;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor () {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  /* 
+    Push está sendo usado como unshift - adicona no início;
+    Esse "unshift nomeado de push" é uma boa estratégia já que não é 
+    preciso reindexar toda a lista devida sua estrutura ser linkada e não
+    indexada como nos arrays tradicionais. caso contrário o unshift e o shift 
+    teriam uma complexidade maior. 
+
+  */
+
+}
