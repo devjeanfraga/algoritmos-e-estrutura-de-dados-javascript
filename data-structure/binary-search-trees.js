50af9ef00efbfa622d6f1f77df6f513d300d0ef3
Jean Fraga <jeandevjs@gmail.com>
2022-09-13 11:00:22 -0300

    study: add insert method

--- data-structure/binary-search-trees.js
+++ data-structure/binary-search-trees.js
@@ -10,4 +10,33 @@ class BinarySearchTree {
   constructor () {
     this.root = null;
   }
+
+  insert (value) {
+    const newNode = new Node(value);
+
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    let current = this.root;
+
+    while (true) {
+      if (value === current.value) return undefined; 
+      if (value < current.value) {
+        if (!current.left) {
+          current.left = newNode;
+          return this; 
+        };
+        current = current.left;
+        
+      } else {
+        if (!current.right) {
+          current.right = newNode;
+          return this; 
+        };
+        current = current.right; 
+      };
+    };
+  };
 }
