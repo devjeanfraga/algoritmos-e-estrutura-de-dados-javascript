8dd110b1e83e014d548620077c7630bee863ee56
Jean Fraga <jeandevjs@gmail.com>
2022-09-20 13:05:39 -0300

    study:  add recursive sinkDown method

--- data-structure/binary-heaps.js
+++ data-structure/binary-heaps.js
@@ -1,6 +1,6 @@
 class MaxBinaryHeaps {
   constructor () {
-    this.values = [40,38,35,32,31,24,30,19];
+    this.values = [40,39,35,38,31,24,30,19,32];
   }
 
   swap (arr, a,b) {
@@ -37,47 +37,52 @@ class MaxBinaryHeaps {
     return max;
 }
 
-sinkDown () {
-
-const helper = (i=0, el, isSwap = true) => {
-    if(!isSwap) return;
-    el = this.values[i];
-    let leftIdx = Math.floor(i*2+1); 
-    let rightIdx = Math.floor(i*2+2);
-    let leftChild = this.values[leftIdx];
-    let rightChild = this.values[rightIdx];
-    
-    if (leftChild > rightChild) {
-      if (el < leftChild ) {
-        isSwap = this.swap(this.values, i,leftIdx) ; 
-        i = leftIdx;
-      } else {
-          isSwap = false
-      } 
-    } else {
-      if (el < rightChild ) {
-        isSwap = this.swap(this.values, i,rightIdx) ;
-        i = rightIdx;
-      } else {
-          isSwap = false
-      }
+  sinkDown () {
+    let i= 0;
+    const helper = ( isSwap = true) => {
+      if(!isSwap) return
+      let el = this.values[i];
+      let leftIdx = Math.floor((i*2)+1), 
+          rightIdx = Math.floor((i*2)+2);
+      let leftChild = this.values[leftIdx] ?? undefined, 
+          rightChild = this.values[rightIdx] ?? undefined; 
+        
+      if (el < leftChild || el < rightChild) 
+          leftChild > (rightChild ?? - Infinity)  
+            ? (isSwap = this.swap(this.values, i, leftIdx), i = leftIdx)
+            : (isSwap = this.swap(this.values, i, rightIdx), i = rightIdx)
+      else isSwap = false;
+        
+      helper(isSwap);   
     }
-    el = this.values[i]
-
-    return helper(i,el, isSwap) ;
-    
-   
-}
-return helper()
-}
+      return helper()
+  }
 }
 
+let heaps = new MaxBinaryHeaps()
+console.log(heaps.extractMax());
+console.log(heaps.extractMax());
+console.log(heaps.extractMax());
+console.log(heaps.extractMax());
+console.log(heaps.extractMax());
+console.log(heaps.extractMax());
+console.log(heaps.extractMax());
+console.log(heaps.extractMax());
+console.log(heaps.extractMax());
+console.log(heaps.extractMax());
+// heaps.extractMax();
 //       40
 //    39    35
 //  38 31 24 30
 //19 32
-// [40,38,35,32,31,24,30,19]
-// [40,39,35,38,31,24,30,19,32]
-let heaps = new MaxBinaryHeaps()
+// [40,39,35,38,31,,24,30,19,32]
+// [39,38,35,32,31,24,30,19]
+// [38,32,35,19,31,24,30]
+// [35,32,30,19,31,24]
+// [32,31,30,19,24]
+// [31,24,30,19]
+// [30,24,19]
+// [24,19]
+// [19]
+// undefined
 
-heaps.extractMax();
