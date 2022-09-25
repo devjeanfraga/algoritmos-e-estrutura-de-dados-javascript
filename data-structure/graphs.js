76c839d6858ce07575d93e4f61c1f173dc34d8c0
Jean Fraga <jeandevjs@gmail.com>
2022-09-25 16:01:53 -0300

    study: add methods

--- data-structure/graphs.js
+++ data-structure/graphs.js
@@ -15,5 +15,29 @@ class GraphAdjacencyList {
     //this.AdjacencyMatrix = [];
   }
 
+  addVertex (vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] =  [];
+  }
+
+  addEdge (vertex1, vertex2 ) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+    return this; 
+  }
+
+  removeEdge (vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter( v => v !== vertex2);
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter( v => v !== vertex1 );
+    //return this.adjacencyList[vertex1] && this.adjacencyList[vertex2]; 
+  }
+
+  removeVertex (vertex) {
+    if (!this.adjacencyList[vertex].lenght) {
+      delete this.adjacencyList[vertex];
+      return;
+    }
+    this.removeEdge(vertex, this.adjacencyList[vertex][0]);
+    this.removeVertex(vertex)
+  }
 
  }
