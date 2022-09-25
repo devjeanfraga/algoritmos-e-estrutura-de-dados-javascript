f301f8754216c387b8f26346c6e7ad35c31b856e
Jean Fraga <jeandevjs@gmail.com>
2022-09-25 16:01:32 -0300

    study: add graph class

---
+++ data-structure/graphs.js
@@ -0,0 +1,19 @@
+/*
+Vertex - a node
+Edge - connection between nodes
+Weighted/Unweighted - values assigned to distances between vertices | valores atribuídos a distâncias entre vértices
+Directed/Undirected - directions assigned to distanced between vertices | direções atribuídas a distanciadas entre vértices
+
+
+*/
+
+
+// Esse grafo possui edges bidirecionais
+class GraphAdjacencyList {
+  constructor () {
+    this.adjacencyList = {};
+    //this.AdjacencyMatrix = [];
+  }
+
+
+ }
