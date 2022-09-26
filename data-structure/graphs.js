c70aaac29bb6abf5fbc253ef234f5198e9286af4
Jean Fraga <jeandevjs@gmail.com>
2022-09-26 19:46:24 -0300

    study: add depthFirst recursive | iterative

--- data-structure/graphs.js
+++ data-structure/graphs.js
@@ -40,4 +40,95 @@ class GraphAdjacencyList {
     this.removeVertex(vertex)
   }
 
+  // create a function that accept a vertex;
+  // create the restore to result and to the value visited;
+  // instance the adjacencyList in a variable;
+  //create a helper function that accept a vertex; 
+
+  // start in index 0; 
+  depthFirstSearchRecursive (start) {
+    let result = [];
+    let isVisited = {};
+    let adjacencyList = this.adjacencyList; // instanciar adjacencyList aqui para ser chamada correntamente no escopo da DFS function helper; 
+
+    (function DFS (vertex) {
+      if (!vertex) return null; 
+      isVisited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach(neighbor => {
+        if(!isVisited[neighbor]) return DFS(neighbor)
+      });
+    })(start);
+
+    return result; 
+  }
+
+  //start in index -1; 
+  depthFirstSearchIterative (vertex) {
+    let stack = [vertex];
+    let result = [];
+    let isVisited = {}; 
+    let currentVertex;
+
+    isVisited[vertex] = true;
+    while (stack.length) {
+        console.log(stack)
+        currentVertex =  stack.pop();
+        result.push(currentVertex);
+        
+        this.adjacencyList[currentVertex].forEach( neighbor => {
+            if(!isVisited[neighbor]){
+                isVisited[neighbor] = true;
+                stack.push(neighbor)
+            }
+        });
+    };
+    return result;    
+  }
+
+  depthFirstSearchIterative2 (vertex) {
+    let stack = [vertex];
+    let result = [];
+    let isVisited = {}; 
+    let currentVertex;
+
+    isVisited[vertex] = true;
+
+    while (stack.length) {
+      console.log(stack)
+      currentVertex =  stack.pop();
+      result.push(currentVertex);
+
+      for (let i = this.adjacencyList[currentVertex].length-1; i >= 0; i--) {
+        let neighbor = this.adjacencyList[currentVertex][i]
+        if(!isVisited[neighbor]){
+          isVisited[neighbor] = true;
+          stack.push(neighbor)
+        }
+      }
+    };
+    return result;    
+  }
+
  }
+
+
+const g = new GraphAdjacencyList();
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
+
+g.addEdge("A","B")
+g.addEdge("A","C")
+g.addEdge("B","D")
+g.addEdge("C","E")
+g.addEdge("D","E")
+g.addEdge("D","F")
+g.addEdge("E","F")
+
+g.depthFirstSearchRecursive("A") // start 0; RESULT: ['A', 'B', 'D', 'E', 'C', 'F']
+g.depthFirstSearchIterative("A") // start -1 RESULT: ['A', 'C', 'E', 'F', 'D', 'B']
+g.depthFirstSearchIterative2("A") // start 0 RESULT: ['A', 'B', 'D', 'E', 'F', 'C']
