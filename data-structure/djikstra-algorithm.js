aed08fb0e758e3bfce28071d72e5d75fdd8c608d
Jean Fraga <jeandevjs@gmail.com>
2022-09-28 20:35:31 -0300

    study: add priorityQueue

---
+++ data-structure/djikstra-algorithm.js
@@ -0,0 +1,13 @@
+class Node {
+  constructor (value, priority) {
+    this.value = value;
+    this.priority=  priority; 
+  }
+}
+
+class PriorityQueue {
+  constructor () {
+    this.values  = [];
+  }
+  
+}
