68bb772f9ec4855533a1ca08157b562a48c4f4fa
Jean Fraga <jeandevjs@gmail.com>
2022-09-28 20:36:02 -0300

    study: add enqueue method

--- data-structure/djikstra-algorithm.js
+++ data-structure/djikstra-algorithm.js
@@ -9,5 +9,35 @@ class PriorityQueue {
   constructor () {
     this.values  = [];
   }
-  
+  swap (arr, idx1, idx2) {
+    let temp = arr[idx1];
+    arr[idx1] = arr[idx2];
+    arr[idx2] =  temp;   
+  }
+
+  enqueue (value, priority) {
+    let newNode = new Node(value, priority); 
+    this.values.push(newNode);
+    if (this.values.length > 1) this.bubbleUp(); 
+    return this; 
+  }
+
+  bubbleUp () {
+    let  i = this.values.length-1; 
+    let el; 
+
+    const helper =  () => {
+      if (i > 0 ) el = this.values[i].priority;
+      else return; 
+      let parentIdx = Math.floor(i-1/2);
+      let parent = this.values[parentIdx]?.priority;
+      
+      if (el < parent) {
+        this.swap(this.values, i, parentIdx);
+        i = parentIdx;
+      } else return; 
+      helper(i);
+    }
+    helper()
+  }
 }
