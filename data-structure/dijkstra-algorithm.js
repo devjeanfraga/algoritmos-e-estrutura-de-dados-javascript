754020500d6ce9d682b2bd597cc372285c3579ef
Jean Fraga <jeandevjs@gmail.com>
2022-09-29 22:51:35 -0300

    study: finally i learn Dijkstra algorithm

--- data-structure/dijkstra-algorithm.js
+++ data-structure/dijkstra-algorithm.js
@@ -4,7 +4,6 @@ class Node {
     this.priority=  priority; 
   }
 }
-
 class PriorityQueue {
   constructor () {
     this.values  = [];
@@ -71,21 +70,17 @@ class PriorityQueue {
     helper();
   }
 }
-
 class WeightedGraph {
   constructor () {
     this.adjacencyList = {}
   }
-
   addVertex(vertex) {
     if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = []
   }
-
   addEdge(start, finish, weight) {
     this.adjacencyList[start].push({node: finish, weight });
     this.adjacencyList[finish].push({node: start, weight });
   }
-
   Dijkstra(start, end) {
     let nodes = new PriorityQueue(); 
     let distances = {};
@@ -97,24 +92,37 @@ class WeightedGraph {
       if (vtex === start ) (distances[vtex] = 0, nodes.enqueue(vtex, 0));
       else (distances[vtex] = Infinity, nodes.enqueue(vtex, Infinity));
       previous[vtex] = null; 
-    }
+    };
 
+    // CORE 
     while (nodes.values.length) {
       priority = nodes.dequeue().value;
-      if (priority === end ) {
 
-      }
+      if (priority === end ) {
+        while (previous[priority]) {
+          path.push(priority);
+          priority = previous[priority]; 
+        };
+        break; 
+      };
 
       if (distances[priority] !== Infinity) {
         for ( let i in this.adjacencyList[priority] ) {
-          let elOfPriority = this.adjacencyList[priority][i];
-          console.log(i)
-          console.log(elOfPriority)
+          let elOfPriority = this.adjacencyList[priority][i]; //{node:B, weight: 4 } | {node: C, weight: 2}...
+          let elVtex = elOfPriority.node; // B | C...
 
-        }
-      } 
-    }
-  }
+          let candidate = distances[priority] + elOfPriority.weight; // 4 | 2..
+          
+          if (candidate < distances[elVtex]) {
+            distances[elVtex] = candidate;
+            previous[elVtex] = priority; 
+            nodes.enqueue(elVtex, candidate); 
+          };
+        };
+      }; 
+    };
+     return path.concat(priority).reverse(); 
+  };
 }
 
 const graph = new WeightedGraph()
@@ -133,4 +141,4 @@ graph.addEdge("C","F",4)
 graph.addEdge("D","E",3)
 graph.addEdge("D","F",1)
 graph.addEdge("E","F",1)
-graph.Dijkstra("A","E")
+graph.Dijkstra("A","E") // [ 'A', 'C', 'D', 'F', 'E' ]
