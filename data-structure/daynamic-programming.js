341fadef764cb683075f2f4c378223870f8be55a
Jean Fraga <jeandevjs@gmail.com>
2022-10-03 21:58:12 -0300

    study: dynamic programming

--- data-structure/daynamic-programming.js
+++ data-structure/daynamic-programming.js
@@ -1,1 +1,24 @@
-/* Overlapping */
+function noDynamicFibonacci (n) {
+ if (n <= 2 ) return 1; 
+ return noDynamicFibonacci(n-1) + noDynamicFibonacci(n-1); 
+}
+
+/* MEMOIZATION */
+function dynamicfibonacci (n, memo = []) {
+  if (memo[n] !== undefined) return memo[n];
+  if (n <= 2) return 1;
+  let res = dynamicfibonacci(n-1) + dynamicfibonacci(n-2);
+  memo[n] = res;
+  return res;
+}
+
+/* TABULATION */
+
+function fibonacciTabulation (n) {
+  if (n <=2) return 1;
+  let fibNums = [0,1,1]
+  for (let i = 3; i < n;  i++) {
+    fibNums[n] = fibNums[i-1] + fibNums[n-2];
+  }
+  return fibNums[n]; 
+}
