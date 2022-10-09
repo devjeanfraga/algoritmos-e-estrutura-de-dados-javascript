08e4c6d040c2f10ee0c8dd402f12cbda10af4db6
Jean Fraga <jeandevjs@gmail.com>
2022-10-09 13:53:26 -0300

    study: add addVertex method

--- data-structure/graph-adjacency-matrix.js
+++ data-structure/graph-adjacency-matrix.js
@@ -1,6 +1,27 @@
 class GraphAdjacencyMatrix {
   constructor () {
-    this.vertex = []
-    this.matrix = []
+    this.vtexIndex = {}
+    this.adjacencyMatrix = []
+  }
+
+  addVertex (vtex) {
+    let vertex = Object.keys(this.vtexIndex);
+    if (vertex.indexOf(vtex) === -1 ) {
+      this.adjacencyMatrix.push([])
+      this.vtexIndex[vtex] = this.adjacencyMatrix.length-1
+    }
+      return { matrix:this.adjacencyMatrix, index: this.vtexIndex};
+  }
+
+  addEdge (vtex1, vtex2) {
+    
   }
 }
+
+const graph =  new GraphAdjacencyMatrix();
+console.log(graph.addVertex("A"));
+console.log(graph.addVertex("B"));
+
+
+
+
