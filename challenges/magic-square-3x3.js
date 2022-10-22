b79f46c505f708b10d854a412b8fcac0b9418ffb
Jean Fraga <jeandevjs@gmail.com>
2022-10-21 22:26:46 -0300

    refactor

--- challenges/magic-square-3x3.js
+++ challenges/magic-square-3x3.js
@@ -16,62 +16,53 @@ function solution(s) {
   let diff, cost = [];
   let current, sum = 0;
 
-  // const sumHelper = ( arr, j, k) => {
-  //   let res;
-  //   if (k % 2 === 0 ) {
-  //   !arr[j][k+1] && !arr[j][k+2]
-  //     ? res = (arr[j][k] + arr[j][k-1] + arr[j][k-2]) 
-  //     : (res = arr[j][k] + arr[j][k+1] + arr[j][k+2])      
-  //   } else res = arr[j][k] + arr[j][k+1] + arr[j][k-1]
-  //   return res;
-  // }
+  const magicSquare = [
+    [8,3,4],
+    [1,5,9],
+    [6,7,2]
+  ];
   
  // HANDLE MIDDLE SQUARE
-  if (s[1][1] !== middle ) {
-    current = s[1][1];
-    s[1][1] = middle;
-    diff = s[1][1] - current;      
-    cost.push(Math.abs(diff));
-  };
+ if (s[1][1] !== magicSquare[1][1] ) {
+  current = s[1][1];
+  s[1][1] = magicSquare[1][1];
+  diff = s[1][1] - current;
+  cost.push(Math.abs(diff));
+ };
 
-
- for (let i = 0; i < 3; i++ ) {
-  if(i % 2 === 0 ) {
-    if ( s[0][i] % 2 === 0 ) {
-      if(s[0][i] === (5 + 3 || 5 - 3) ) {
-        let newEnd = 5 + 1 
-        s[0][2] === newEnd ? s[0][2] : newEnd =  5 - 1;
-        s[0][2] === newEnd ? s[0][2] : s[0][2] = newEnd; 
-      } else {
-        let newstart =  5 + 1;
-        s[0][i] === newstart ? s[0][i] : newstart =  5 - 1;
-        s[0][i] === newstart ? s[0][i] : s[0][0] =  newstart; 
-      }   
-    } else { //SE O VALOR FOR IMPAR
-      let arr = s[0]
-      if (i === 0 && arr[i] === arr[i+2]) {
-        arr[i] = arr[i] + 1;
-        arr[i+2] = arr[i+2] - 1;
-        arr[i+2] === 0 ? arr[i+2] = arr[i+2] + 4: arr[i+2];
-        
-      } else if (i % 2 === 0) (s[0][i] < 9 && s[0][i] >= 1) ? s[0][i] = s[0][i] + 1 : s[0][i] = s[0][i] - 1;
-    }
-  } else s[0][i] = 15 - (s[0][i-1] + s[0][i+1]);
-}
   
-  return s;
+  // Rotation
+  const swap = (arr, pos = 0) => {
+
+    [arr[pos][2], arr[pos+2][0]] = [arr[pos+2][0], arr[pos][2]];
+     
+    [arr[pos][1], arr[pos+1][0]] = [arr[pos+1][0], arr[pos][1]];
+     
+    [arr[pos+1][2], arr[pos+2][1]] = [arr[pos+2][1], arr[pos+1][2]];
+ 
+     arr.map( i =>  i.reverse())
+     cost.push(arr)
+   
+  }
+
+ const flatten = (arr) => {
+   let list = [];
+   for (let i = 0; i < arr.length; i++) {
+     if(Array.isArray(arr[i])) list = list.concat(flatten(arr[i]));
+     else list.push(arr[i])
+   }
+
+  return list; 
+ }
+ swap(s)
+ console.log(s)
+
+ return cost;
 }
-
-const matrix = [
-[5,3,4],
-[1,5,8],
-[6,4,2]
-]
-
-const matrix2 = [
-[9,3,4],
-[7,5,8],
-[6,9,2]
+const matrix6 = [
+[8,1,6],
+[3,5,7],
+[4,9,2]
 ];
 
-console.log(solution(matrix));
+solution(matrix6);
