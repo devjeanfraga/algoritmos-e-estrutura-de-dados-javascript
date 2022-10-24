6ac8512646c35d57ccbc571d066e7cae0087a156
Jean Fraga <jeandevjs@gmail.com>
2022-10-23 21:33:06 -0300

    refactor

--- challenges/magic-square-3x3.js
+++ challenges/magic-square-3x3.js
@@ -12,52 +12,58 @@ given a 3X3 matrix of integer inclusive range [1,9]
 */
 
 function solution(s) {
-  let middle = 5;
-  let diff, cost = [];
-  let current, sum = 0;
-
-  const magicSquare = [
-    [8,3,4],
-    [1,5,9],
-    [6,7,2]
-  ];
+  let middle = 5, cost = 0, squares = [];
+  let magicSquare = [ 
+   [8,3,4], 
+   [1,5,9], 
+   [6,7,2] ];
   
- // HANDLE MIDDLE SQUARE
- if (s[1][1] !== magicSquare[1][1] ) {
-  current = s[1][1];
-  s[1][1] = magicSquare[1][1];
-  diff = s[1][1] - current;
-  cost.push(Math.abs(diff));
- };
-
-  
-  // Rotation
-  const swap = (arr, pos = 0) => {
-
-    [arr[pos][2], arr[pos+2][0]] = [arr[pos+2][0], arr[pos][2]];
-     
-    [arr[pos][1], arr[pos+1][0]] = [arr[pos+1][0], arr[pos][1]];
-     
-    [arr[pos+1][2], arr[pos+2][1]] = [arr[pos+2][1], arr[pos+1][2]];
- 
-     arr.map( i =>  i.reverse())
-     cost.push(arr)
-   
+  //flatten
+  const flatten = (arr) => {
+    let list = [];
+    for (let i = 0; i < arr.length; i++) 
+     if (Array.isArray(arr[i])) list = list.concat(flatten(arr[i]));
+     else list.push(arr[i])
+    return list; 
   }
 
- const flatten = (arr) => {
-   let list = [];
-   for (let i = 0; i < arr.length; i++) {
-     if(Array.isArray(arr[i])) list = list.concat(flatten(arr[i]));
-     else list.push(arr[i])
-   }
+  // Rotation to generate magic squares
+  const generateMagicSquares = ( magicSqr = magicSquare, counter = 1, pos = 0) => {
+   // Deep Copy
+   let arr = JSON.parse(JSON.stringify(magicSqr));
+   let temp;
+   
+   if (counter > 8) return;
+   if (counter > 4) magicSqr = magicSqr.map(i => i.reverse()) 
+   
+   temp = arr[pos][2];
+   arr[pos][2] = arr[pos+2][0];
+   arr[pos+2][0] = temp; 
 
-  return list; 
- }
- swap(s)
- console.log(s)
+   temp = arr[pos][1];
+   arr[pos][1] = arr[pos+1][0];
+   arr[pos+1][0] = temp;
 
- return cost;
+   temp = arr[pos+1][2];
+   arr[pos+1][2] = arr[pos+2][1];
+   arr[pos+2][1] = temp; 
+
+   counter < 5 ? arr = arr.map(i => i.reverse()) : arr ;
+   squares.push(arr)
+   
+   counter = counter+1
+   generateMagicSquares(arr, counter);
+  }
+
+  s = flatten(s) 
+  generateMagicSquares()
+
+  for (let i = 0; i < squares.length; i++) {
+   
+  }
+  
+
+ return squares;
 }
 const matrix6 = [
 [8,1,6],
