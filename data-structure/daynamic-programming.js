d8789ae3d2cc380d51853fb809e89e707ec27c16
Jean Fraga <jeandevjs@gmail.com>
2022-12-17 09:39:57 -0300

    refactor fibonacciTabulation

--- data-structure/daynamic-programming.js
+++ data-structure/daynamic-programming.js
@@ -19,8 +19,9 @@ function dynamicfibonacci (n, memo = []) {
 function fibonacciTabulation (n) {
   if (n <=2) return 1;
   let fibNums = [0,1,1]
-  for (let i = 3; i < n;  i++) {
-    fibNums[n] = fibNums[i-1] + fibNums[n-2];
+  for (let i = 3; i <= n;  i++) {
+    fibNums[i] = fibNums[i-1] + fibNums[i-2];
   }
   return fibNums[n]; 
 }
+
