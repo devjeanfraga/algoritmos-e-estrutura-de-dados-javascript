7cf34003d97fdfe41d9b35bdd19e426360a61f20
Jean Fraga <72657296+devjeanfraga@users.noreply.github.com>
2023-10-05 08:03:30 -0300

    Update sort-merge-sort.js
    add explain about merge sort

--- sort/sort-merge-sort.js
+++ sort/sort-merge-sort.js
@@ -9,6 +9,24 @@
   Time commplexity(Worst) - O(n logn )
   Space commplexity - O(n)
 
+A complexidade de tempo do Merge Sort éθ(Nlog(N)) em todos os 3 casos (pior, médio e melhor), pois o merge sort sempre divide a matriz em duas metades e leva tempo linear para mesclar as duas metades.
+
+Espaço Auxiliar: O(N), Na classificação por mesclagem, todos os elementos são copiados em uma matriz auxiliar. Portanto, N espaço auxiliar é necessário para classificação por mesclagem.
+
+Aplicações de classificação por mesclagem:
+Classificando grandes conjuntos de dados: a classificação por mesclagem é particularmente adequada para classificar grandes conjuntos de dados devido à sua complexidade de tempo garantida no pior caso de O (n log n).
+Classificação externa: a classificação por mesclagem é comumente usada na classificação externa, onde os dados a serem classificados são muito grandes para caber na memória.
+Classificação personalizada: a classificação por mesclagem pode ser adaptada para lidar com diferentes distribuições de entrada, como dados parcialmente classificados, quase classificados ou completamente não classificados.
+
+Vantagens da classificação por mesclagem:
+Estabilidade : Merge sort é um algoritmo de classificação estável, o que significa que mantém a ordem relativa de elementos iguais na matriz de entrada.
+Desempenho garantido no pior caso: a classificação por mesclagem tem uma complexidade de tempo de pior caso de O(N logN), o que significa que tem um bom desempenho mesmo em grandes conjuntos de dados.
+Paralelizável : Merge sort é um algoritmo naturalmente paralelizável, o que significa que pode ser facilmente paralelizado para aproveitar vários processadores ou threads.
+
+Desvantagens da classificação por mesclagem:
+Complexidade de espaço: a classificação por mesclagem requer memória adicional para armazenar as submatrizes mescladas durante o processo de classificação. 
+Não no local: a classificação por mesclagem não é um algoritmo de classificação no local, o que significa que requer memória adicional para armazenar os dados classificados. Isto pode ser uma desvantagem em aplicações onde o uso de memória é uma preocupação.
+Nem sempre é ideal para conjuntos de dados pequenos: para conjuntos de dados pequenos, a classificação por mesclagem tem uma complexidade de tempo maior do que alguns outros algoritmos de classificação, como a classificação por inserção. Isso pode resultar em desempenho mais lento para conjuntos de dados muito pequenos.
 */
 
 
