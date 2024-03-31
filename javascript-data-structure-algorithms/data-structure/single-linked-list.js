87647cccf4740497022fcc85e2c5e1b93d7609ec
Jean Fraga <jeanfragadeveloper@gmail.com>
2024-03-31 09:33:56 -0300

    data structure: single linked list

---
+++ javascript-data-structure-algorithms/data-structure/single-linked-list.js
@@ -0,0 +1,149 @@
+
+class Node {
+  constructor ( value ) {
+    this.value = value;  
+    this.next = null; 
+  };
+}
+
+
+class SinglyLinkedList  {
+  constructor () {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  };
+  // adiciona no final da lista 
+  push (value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    };
+    ++this.length;
+    return this;
+  };
+  
+  // remove do final da lista
+  pop () {
+    if(!this.head) return undefined; 
+    let current = this.head;
+    let newTail =  current;
+
+    while (current.next !== null) {
+        newTail = current 
+        current =  current.next 
+    };
+
+    this.tail = newTail; 
+    this.tail.next = null; 
+    this.length-- 
+    if (this.length === 0 ) { 
+      this.head = null;
+        this.tail = null;
+    };
+    return current; 
+  };
+
+  // remove do inicio da lista
+  shift () {
+    if (!this.head) return undefined;
+    let currentHead = this.head;
+    this.head =  currentHead.next;
+    this.length--;
+    if (this.length === 0 ) this.tail = null;
+    return currentHead.value;
+  };
+  
+  // adiciona no inico da lista
+  unshift (value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail =  this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    };
+    this.length++;
+    return this;
+  };
+  
+  get (index) {
+    if (index < 0 || index >= this.length ) return null; 
+    let current =  this.head;
+    let counter = 0;
+    while (index !== counter) {
+      current = current.next;
+      counter++; 
+    };
+    return current; 
+  };
+
+  set (index, value) {
+    let foundNode = this.get(index);
+    if (foundNode) { 
+      foundNode.value = value;
+      return true;
+    };
+    return false;   
+  };
+
+  insert (index, value) {
+    if (index < 0 || index > this.length ) return false;
+    if ( index === this.length ) return !!this.push(value);
+    if (index === 0 ) return !!this.unshift(value);
+
+    let newNode = new Node(value);
+    let prev = this.get(index-1);
+    let temp =  prev.next; 
+    prev.next = newNode;
+    newNode.next = temp;
+    this.length++;
+    return true; 
+  };
+
+  remove (index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length -1) return this.pop();
+
+    let previousNode = this.get(index -1);
+    let removed = previousNode.next;
+    previousNode.next = removed.next; 
+    this.length--;
+    return removed; 
+  };
+
+  reverse () {
+    let node = this.head;  
+    this.head = this.tail; 
+    this.tail = node; 
+    let prev = null;    
+    let next; 
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;  
+      node.next = prev; 
+      prev = node; 
+      node = next; 
+    };
+    return this; 
+  };
+
+  print () {
+    if (this.length === 0 ) return undefined;
+    let arr = [];
+    let current = this.head;
+    while (current) {
+      arr.push(current.value)
+      current = current.next
+    };
+    return arr; 
+  };
+};
+
+
+
---
+++ javascript-data-structure-algorithms/data-structure/the-wild-west-exercises/single-linked-list-exercises.js
@@ -0,0 +1,122 @@
+class Node {
+  constructor (val) {
+    this.val = val;
+    this.next = null; 
+  };
+};
+
+class SingleLinkedList {
+  constructor () {
+    this.head = null;
+    this.tail = null; 
+    this.size = 0;
+  };
+
+  push (val) {
+    let newNode = new Node(val); 
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail =  newNode; 
+    }
+    ++this.size;
+    return this; 
+  };
+
+  pop () {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail =  current; 
+
+    while (current.next !== null) (newTail = current, current = current.next )
+
+    this.tail = newTail;
+    this.tail.next = null; 
+    --this.size;
+    
+    if (this.size === 0) {
+      this.head = null;
+      this.tail = null; 
+    }
+    return current; 
+  };
+
+  shift () {
+    if (!this.head) return undefined;
+    let shiftted =  this.head; 
+    this.head = shiftted.next; 
+    shiftted.next = null; 
+    --this.size;
+    if (this.size === 0 ) (this.head = null, this.tail = this.head)
+    return shiftted; 
+  }
+
+  unshift (val) {
+    let newNode = new  Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    ++this.size;
+    return this; 
+  }
+
+  get (index) {
+    //verificar se o index está entre o tamanho da lista;
+    if (index < 0 || index >= this.size) return null;
+
+    let current = this.head; 
+    let counter  = 0; 
+
+    while (index !== counter) {
+      current = current.next
+      counter++;
+    }
+
+    return current; 
+  }
+
+  set (index, val) {
+    let foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true; 
+    }
+    return false; 
+  }
+
+  insert (index, val) {
+    if (index < 0 || index >= this.size) return null;
+    if (index === 0 ) return !!this.unshift(val);
+    if (index ===  this.size) return !!this.push(val);
+
+    let newNode = new  Node(val);
+    let frontNode = this.get(index-1);
+    let temp = frontNode.next;
+    frontNode.next = newNode;
+    newNode.next = temp; 
+    ++this.size;
+    return true; 
+  }
+
+  reverse () {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail =  node;
+    let previous = null, next; 
+    for (let i = 0; i < this.size; i++) {
+      next = node.next;
+      node.next = previous;
+      previous = node;
+      node = next;
+    }
+  }
+};
+
+// 1,2,3,4,5
+// 5,3,4,1,2
